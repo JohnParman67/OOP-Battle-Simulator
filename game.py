3e7473205068b8a8453a88764b7b596295a268f8
JohnParman67 <johnparman9@gmail.com>
2026-09-11 10:12:01 -0500

    Add Second Goblin Enterance

--- game.py
+++ game.py
@@ -1,18 +1,20 @@
 from goblin import Goblin
 
 
-ARENA_NAME = "The Iron Lung"
+ARENA_NAME = "Tung Tung SaGame"
 
 
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
-    print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
+    print("✌️ 😂")
     print("The gates are opening...")
 
-    goblin = Goblin("Gribble")
+    goblin = Goblin("GoblinDude")
+    goblin2 = Goblin("GoblinGuy")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
+    print(f"{goblin2.name} enters the arena with {goblin2.health} health.")
     print("But no hero has answered the call... yet.")
 
 
